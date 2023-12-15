f99108e5ac82381d4cfa7c40a526f614d95ba8bc
IamHarsh05 <harshchaudhari592@gmail.com>
2023-12-16 00:44:56 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './Componet/Login/login';
 import Home from './Componet/Home/home';
+import TopMenu from './Componet/Header/Dashboard_Top';
+import Upload from './Componet/Upload/Upload';
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-      <Home/>
+    <div className='flex flex-col h-screen'>
+      <div className='w-screen'>
+        <TopMenu/>
+      </div>
+      <div className='overflow-y-scroll'>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={
+              <>
+                <Upload/>
+                <Home />
+              </>
+              } />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
---
+++ src/Componet/Upload/Upload.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+const Upload = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
+  const handleImageChange = (e) => {
+    setIsFormVisible(!isFormVisible);
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setSelectedImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleSaveImage = () => {    
+    setIsFormVisible(!isFormVisible);
+    if (selectedImage) {
+      // Save image to local storage
+      const images = JSON.parse(localStorage.getItem('images')) || [];
+      images.push(selectedImage);
+      localStorage.setItem('images', JSON.stringify(images));
+
+      // Reset selected image
+      setSelectedImage(null);
+    }
+  };
+
+  return (
+    <div className='relative p-4 bg-slate-200'>
+        <label htmlFor='image'>Upload</label>
+      <input type="file" id='image' className='hidden' accept="image/*" onChange={handleImageChange} />
+      {isFormVisible && 
+        <div className='absolute flex flex-col justify-center p-4 bg-slate-100'>
+            <img src={selectedImage} alt="Selected" />
+            <button onClick={handleSaveImage}>Save Image</button>
+        </div>
+      }
+    </div>
+  );
+};
+
+export default Upload;
